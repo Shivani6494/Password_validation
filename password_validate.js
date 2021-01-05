40177c268ca5c6114a325d1d356c1ce13ef04b6a
shivani-2020 <bhattshivani64@gmail.com>
2021-01-05 18:02:25 -0500

    first commit

---
+++ password_validate.js
@@ -0,0 +1,15 @@
+
+function CheckPassword(inputtxt) 
+{ 
+var passw=  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{6,15}$/;
+if(inputtxt.value.match(passw)) 
+{ 
+alert('Correct, try another...')
+return true;
+}
+else
+{ 
+alert('Wrong...!')
+return false;
+}
+}
